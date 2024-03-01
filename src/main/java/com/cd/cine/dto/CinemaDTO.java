7b901f497de03f34b7bd8627109a6efef1af4690
cristiannd <cristian.donalicio@gmail.com>
2024-03-01 12:10:02 -0300

    Add CinemaDTO

---
+++ src/main/java/com/cd/cine/dto/CinemaDTO.java
@@ -0,0 +1,17 @@
+package com.cd.cine.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class CinemaDTO {
+
+  private Long id;
+
+  private String name;
+}
